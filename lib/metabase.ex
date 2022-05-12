e19baeddee543de16b1f81a0db56bc472d10a558
Anthony Smith <420061+anthonator@users.noreply.github.com>
2022-05-12 06:43:45 -0400

    🥇

---
+++ lib/metabase.ex
@@ -0,0 +1,2 @@
+defmodule Metabase do
+end
---
+++ mix.exs
@@ -0,0 +1,23 @@
+defmodule Metabase.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :metabase,
+      version: "0.0.0",
+      elixir: "~> 1.13",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    []
+  end
+end
