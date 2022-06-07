008cb220ce8cdb8124b4230910220a31360b794d
Anthony Smith <420061+anthonator@users.noreply.github.com>
2022-06-07 13:34:57 -0400

    Support elixir 1.11+

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Metabase.MixProject do
     [
       app: :metabase,
       version: "0.0.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
